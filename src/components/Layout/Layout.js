9cfd95f2aa04f9dada6991f458b90ed5b80988b0
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-11-17 01:27:56 +0100

    Initial layout structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AuxComp from '../../hoc/AuxComp';
+
+const layout = () => {
+    return(
+        <AuxComp>
+            <p>Some text</p>
+            <p>Some other text</p>
+        </AuxComp>
+    )
+}
+
+export default layout;
