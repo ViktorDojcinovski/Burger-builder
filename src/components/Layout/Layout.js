979756944bf10843015399d6645fb25ff6e24823
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-11-17 02:04:37 +0100

    Initial layout structure #2

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import AuxComp from '../../hoc/AuxComp';
+import classes from './Layout.css';
 
 const layout = (props) => {
     return(
         <AuxComp>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={classes.Content}>
                 { props.children }
             </main>
         </AuxComp>
