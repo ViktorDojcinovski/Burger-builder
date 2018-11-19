6d46c417675d26a21ae29fd0c588ee63de586153
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-11-19 16:09:57 +0100

    Build controls ready and show price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,8 +14,14 @@ const buildControls = (props) => {
     
     return ( 
         <div className={classes.BuildControls}>
+            <p>Current price of the burger: <strong>{props.price.toFixed(2)} $</strong></p>
             {controls.map((ctrl) => {
-                return <BuildControl key={ctrl.label} label={ctrl.label} />
+                return <BuildControl 
+                        key={ctrl.label} 
+                        label={ctrl.label}
+                        added={() => props.ingredientAdded(ctrl.type)}
+                        removed={() => props.ingredientRemoved(ctrl.type)}
+                        disabled={props.disabled[ctrl.type]}/>
             })}
         </div>
     )
